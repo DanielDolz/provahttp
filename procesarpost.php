e8f4cb1fabafdec4d252a0a128b1d83ddf9f8b9a
danieldolz <danieldolz@iesebre.com>
2016-10-11 19:13:55 +0200

    formulari: API REST

--- procesarpost.php
+++ procesarpost.php
@@ -1,12 +1,24 @@
 <?php
 
-echo "Hola" . $_POST['nom'];
+var_dump($_SERVER[REQUEST_METHOD]);
 
-var_dump($_POST);
-var_dump($_FILES);
+//echo "Hola" . $_POST['nom'];
+//
+//var_dump($_POST);
+//var_dump($_FILES);
+//
+//// $_POST['name']
+//
+//// --Laravel--
+//// Request -> input('name')
+//// Response
 
-// $_POST['name']
-
-// --Laravel--
-// Request -> input('name')
-// Response
+//API REST
+//
+//Recurs: task
+//
+//CREATE   -> POST miapi.com/task -> Dades:Formulari
+//RETRIEVE -> GET miapi.com/task{id} -> Un sol recurs (NO DADES)
+//            GET miapi.com/task     -> Tots els recursos (NO DADES)
+//UPDATE   -> PUT miapi.com/task{id} -> Dades:Formulari
+//DELETE    -> DELETE miapi.com/task{id}  -> NO DADES
