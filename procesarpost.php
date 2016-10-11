ef73a1bfc56983901e76b99e703ef98b27c2033b
danieldolz <danieldolz@iesebre.com>
2016-10-11 17:54:27 +0200

    formulari: get/post

--- procesarpost.php
+++ procesarpost.php
@@ -1,1 +1,12 @@
-Soc procesar formulari!
+<?php
+
+echo "Hola" . $_POST['nom'];
+
+var_dump($_POST);
+var_dump($_FILES);
+
+// $_POST['name']
+
+// --Laravel--
+// Request -> input('name')
+// Response
